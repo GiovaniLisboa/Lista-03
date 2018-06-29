899ec3e7bd7c3e8eb6b2e966c5d3ee2edd27e044
Giovani Lisboa <giovanilisboa94gmail.com>
2018-06-29 13:36:47 -0300

    a

--- Questão_1.c
+++ Questão_1.c
@@ -1,43 +1,71 @@
 //Giovani Lisboa e Hana Gabrielle
 #include<stdio.h>
+#include<stdlib.h>
 
-typedef struct arvore {
+typedef struct no {
    int	chave;
    int	conteudo;
    struct reg *esq, *dir; 
-} no;
+} no; //Nós para criar a árvore de busca com chave; conteúdo e ponteiros para os filhos direito e esquerdo
 
-no * busca (no noTemp, int ) {
-    if (noTemp == NULL || r->chave == busca)
-       return noTemp;
-    if (noTemp->chave > busca)
-       return busca (noTemp->esq, busca);
+typedef struct no *arvore;
+
+/*struct arvore * criaArvore () {
+	return NULL;
+}*/
+
+void insere (arvore arvTemp, int chaveTemp) {    
+	if (arvTemp == NULL) {
+    arvTemp = malloc (sizeof (arvore));
+    arvTemp->chave = chaveTemp;
+    arvTemp->esq = arvTemp->dir = NULL;
+    printf("%d\n", chaveTemp);
+	}
+    else if (arvTemp->chave > chaveTemp) 
+    	insere (arvTemp->esq, chaveTemp);
+    else if (arvTemp->chave < chaveTemp) 
+		insere (arvTemp->dir, chaveTemp);
+	else printf ("Elemento existente");
+} //Função para inserir um novo nó na árvore e atribuir a chave do vetor
+
+void localiza (arvore arvTemp, int busca) {
+	if (arvTemp == NULL)
+    	return NULL;
+	if (arvTemp->chave == busca)
+       return arvTemp;
+    if (arvTemp->chave > busca)
+		localiza (arvTemp->esq, busca);
     else
-       return busca (noTemp->dir, busca);
+    	localiza (arvTemp->dir, busca);
+} //Função recursiva para testar as chaves de cada nó
+
+
+void exibe (arvore arvTemp) {
+	if (arvTemp != NULL) {
+	printf("<");
+  	printf("%d ", arvTemp->chave); // exibe a chave
+	}
+	if (arvTemp->esq != NULL)
+		exibe(arvTemp->esq);
+	if (arvTemp->dir != NULL)
+    	exibe(arvTemp->dir);
+  	printf(">");	
 }
 
-arvore insere (arvore arvTemp, no *novo) {  
-    if (arvTemp == NULL)
-		return novo;
-    if (arvTemp->chave > novo->chave) 
-    	arvTemp->esq = insere (arvTemp->esq, novo);
-    else 
-		arvTemp->dir = insere (arvTemp->dir, novo);
-    return arvTemp;
-}
-
-main(){
-	int raiz;
+main() {
 	int R[23] = {86, 11, 7, 35, 170, 2, 120, 21, 88, 152, 31, 12, 181, 28, 134, 70, 1, 85, 25, 77, 10, 128, 198};
+	int i;
 	
-	raiz = R[0];
-	for (int i=1; i <= 22; i++){
-		if R[i]<raiz{
-		raiz = R[i];
-		}
-	}
-	
-	for (int i=0; i <= 22; i++){
+	arvore binaria = NULL;
 		
+	int raiz = 70; //média é 69 e é o mais próximo
+	
+	insere (binaria, raiz);
+	
+	for(i = 0; i <= 22; i++){
+		if (R[i] != 70)
+			insere (binaria, R[i]);
 	}
+	
+	exibe (binaria);
 }
---
+++ Questão_2.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+
+int checar (int *temp){
+	int i;
+	for (i = 0; i < 9; i++) {
+		if (temp[i] > temp[i+1]){
+			return 1;
+			break;
+		}
+	}
+	return 0;
+}
+
+void troca(int vetor[], int tamanho){
+	int i, aux, cont;
+	
+	while (checar(vetor)){
+		for (i = 0; i < (tamanho - 1); i++) {
+			if (vetor[i] > vetor[i+1]){
+				aux = vetor[i];
+				vetor[i] = vetor[i+1];
+				vetor[i+1] = aux;
+				cont++;
+			}
+		}
+	}
+
+	printf("\nLista apos ordenacao por troca (Trocas: %d)\n", cont);
+	for (i = 0; i <= 9; i++){
+		printf("Lista[%d]: %d\n", i, vetor[i]);
+	}
+}
+
+void radixsort(int vetor[], int tamanho) {
+	if (checar(vetor)) {
+	    int i;
+	    int *pont;
+	    int base = 1;
+		pont = (int *)calloc(tamanho, sizeof(int));
+
+		int maior = vetor[0];
+    	for (i = 0; i < tamanho; i++) {
+        	if (vetor[i] > maior)
+    	   		maior = vetor[i];
+    	}
+
+    	while (maior/exp > 0) {
+        	int bucket[10] = { 0 };
+    		for (i = 0; i < tamanho; i++)
+    	    	bucket[(vetor[i] / base) % 10]++;
+    		for (i = 1; i < 10; i++)
+    	    	bucket[i] += bucket[i - 1];
+	    	for (i = tamanho - 1; i >= 0; i--)
+	    	    pont[--bucket[(vetor[i] / base) % 10]] = vetor[i];
+	    	for (i = 0; i < tamanho; i++)
+	    	    vetor[i] = b[i];
+	    	base *= 10;
+	    }
+
+    	free(b);
+	}
+	
+	printf("\nLista apos ordenacao por radixsort\n");
+	for (i = 0; i <= 9; i++){
+		printf("Lista[%d]: %d\n", i, vetor[i]);
+	}
+}
+
+main (){
+	int i, trocas = 0;
+	int TAMANHO = 10;
+	
+	int lista[10] = {4, 8, 2, 15, 24, 1, 3, 12, 28, 10};
+	
+	/*for (i = 0; i <= 9; i++){
+		printf("Insira um valor: ");
+		scanf(lista[i]);
+		printf("\n");
+	}*/
+	
+	printf("Lista original\n");
+	for (i = 0; i <= 9; i++){
+		printf("Lista[%d]: %d\n", i, lista[i]);
+	}
+	
+	troca(lista, TAMANHO);
+	
+	radixsort(lista, TAMANHO);
+		
+}
